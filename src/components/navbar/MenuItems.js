a33f8e8b9e5e1a1fdf43392ff562b767247c84ee
Raja Babu <rjbabu1309@gmail.com>
2020-09-17 12:06:39 +0530

    first update

---
+++ src/components/navbar/MenuItems.js
@@ -0,0 +1,37 @@
+export const MenuItems = [
+    {
+        title: 'Class VI',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Class VII',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Class VIII',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Class IX',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Class X',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Class XI',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Class XII',
+        url: '#',
+        cName: 'nav-links'
+    }
+]
--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,11 +1,33 @@
 import React, { Component } from 'react';
-
-
+import './Navbar.css';
+import { MenuItems} from './MenuItems';
 class Navbar extends Component {
+
+    state = {clicked: false}
+
+    handleClick =() => {
+        this.setState({ clicked: !this.state.clicked})
+    }
+
     render() {
         return (
-            <nav>
-                <h1>Welcome to RAP Classes</h1>
+            <nav className="NavbarItems">
+                {/* <h1>Welcome to RAP Classes</h1> */}
+                <h1 className="navbar-logo">RAP Classes<i className="fab fa-react"></i></h1>
+                <div className="menu-icon" onClick={this.handleClick}>
+                    <i className={this.state.clicked ? 'fas fa-times' : 'fa fa-bars'}></i>
+                </div>
+                <ul className={this.state.clicked ? 'nav-menu active': 'nav-menu'}>
+                {MenuItems.map((item, index) => {
+                            return (
+                               <li key={index}>
+                                   <a href={item.url} className={item.cName}>
+                                        {item.title}
+                                    </a>
+                               </li>
+                            )
+                        })}
+                </ul>
             </nav>
         )
     }
