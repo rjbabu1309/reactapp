4b0e5316a45439fec5d76e18fe1aa4971342b9ef
Raja Babu <rjbabu1309@gmail.com>
2020-09-22 08:59:02 +0530

    first update

--- src/components/Add/add.js
+++ src/components/Add/add.js
@@ -5,7 +5,8 @@ class Add extends Component {
     render () {
         return(
             <div className="add">
-                <h1>This under construction Site is for sale.</h1>
+                <h1>This Site is under construction.</h1>
+                <h2>Comming Soon.....</h2>
                 <h3>contact at sabhijeet410@gmail.com</h3>
             </div>
         )
