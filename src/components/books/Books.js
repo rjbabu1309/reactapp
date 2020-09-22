7fd5abc0a4e8dfda26ae325719eea3d8a1347119
Raja Babu <rjbabu1309@gmail.com>
2020-09-22 08:54:15 +0530

    first update

---
+++ src/components/books/Books.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class Books extends Component {
+    render() {
+        return (
+            <div className="main">
+                <h2></h2>
+            </div>
+        )
+    }
+}
+
+export default Books;
